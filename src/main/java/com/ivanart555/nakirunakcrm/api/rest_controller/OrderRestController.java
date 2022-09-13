7eb29a0d6649af7b71f470878e33cb69cb5b385c
Ivan Artsiushyn <ivanart555@gmail.com>
2022-09-13 15:07:51 +0300

    Change notifications formatting

--- src/main/java/com/ivanart555/nakirunakcrm/api/rest_controller/OrderRestController.java
+++ src/main/java/com/ivanart555/nakirunakcrm/api/rest_controller/OrderRestController.java
@@ -62,8 +62,9 @@ public class OrderRestController {
 
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
         telegramBot.execute(SendMessage.builder().chatId(env.getProperty("telegram.bot.chatid")).text("Новая Замова №" + order.getPublicId() + " ад " +
-                order.getTimestamp().format(formatter) + ' ' + "." + order.getCustomer().getName() + ". " + order.getCustomer().getPhoneNumber() +". "+
-                order.getDestination().getName() +"." + ' ' + order.getCustomerComment()).build());
+                order.getTimestamp().format(formatter) + System.lineSeparator() + order.getCustomer().getName() +
+                System.lineSeparator() + order.getCustomer().getPhoneNumber() + System.lineSeparator() +
+                order.getDestination().getName() + System.lineSeparator() + order.getCustomerComment()).build());
 
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(id).toUri();
         return ResponseEntity.created(uri).build();
